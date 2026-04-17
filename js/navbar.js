b50e4365156371d94c2ed717b242e70c8df9e248
fsalamanca2000 <juanf.salamancag@konradlorenz.edu.co>
2026-04-16 20:03:16 -0500

    "Cambios en el header, agregar tildes e imagenes y quitar decoraciones innecesarias en los links"

--- js/navbar.js
+++ js/navbar.js
@@ -3,10 +3,10 @@ window.addEventListener("DOMContentLoaded", () => {
     const session = JSON.parse(localStorage.getItem("session"));
 
     if (session) {
-        // limpiar botones actuales
         nav.innerHTML = `
             <a href="index.html">Inicio</a>
             <a href="camera.html">Cámara</a>
+            <a href="practice.html">Prácticas</a>
             <span style="margin-left:20px;">👤 ${session.usuario.nombre}</span>
             <button id="logoutBtn" class="btn-nav">Salir</button>
         `;
@@ -16,4 +16,12 @@ window.addEventListener("DOMContentLoaded", () => {
             window.location.href = "login.html";
         });
     }
+    //Mantener Activo la pagina en la que se esta presente
+    const currentPage = window.location.pathname.split("/").pop();
+
+    document.querySelectorAll(".navbar a").forEach(link => {
+        if (link.getAttribute("href") === currentPage) {
+            link.classList.add("active");
+        }
+    });
 });
